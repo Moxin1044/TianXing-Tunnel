7dfb57ece7293525d2551aaf7302722a35e31dc0
末心 <1044631097@qq.com>
2026-05-26 06:28:35 +0800

    build: 将版本号更新至1.0.2
    仅修改了build.bat中的版本配置项，将版本从1.0.1升级到1.0.2

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 REM ==================== Version Configuration ====================
-set VERSION=1.0.1
+set VERSION=1.0.2
 
 REM ==================== Project Paths ====================
 set PROJECT_DIR=%~dp0
